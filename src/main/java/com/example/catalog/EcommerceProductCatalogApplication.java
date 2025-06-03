6f054ce1c27fbecae4567991ece6edebf0f4325e
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:25:16 +0530

    Create EcommerceProductCatalogApplication.java

---
+++ src/main/java/com/example/catalog/EcommerceProductCatalogApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class EcommerceProductCatalogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProductCatalogApplication.class, args);
+    }
+}
